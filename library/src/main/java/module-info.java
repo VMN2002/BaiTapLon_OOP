d6bcea509463cec1b9522a2dc7a5d5c2859ec346
VMN2002 <satella1306@gmail.com>
2025-04-24 16:48:16 +0700

    First Commit

---
+++ library/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.library {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.library to javafx.fxml;
+    exports org.example.library;
+}
